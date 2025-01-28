56f713ca2e97a1e2f5c628ada6fa32a9cbd03485
Charles Mendoza <charles.mendoza@ext.essilor.com>
2025-01-28 18:13:09 +0800

    delete storybook-static folder

--- storybook-static/assets/Input.stories-CQRSKgwG.js
+++
@@ -1,5 +0,0 @@
-import{I as a}from"./Input-DiOAHkWd.js";import"./jsx-runtime-z8MfsBtr.js";import"./index-C9rmetsa.js";import"./clsx-B-dksMZM.js";const c={title:"Components/Input",component:a,tags:["autodocs"],argTypes:{}},e={args:{placeholder:"Lorem ipsum"}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    placeholder: "Lorem ipsum"
-  }
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,c as default};
